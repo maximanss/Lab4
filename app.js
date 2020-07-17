fa06fb2ff73706b9a8f589a55aedb0f32edd32de
EC2 Default User <ec2-user@ip-172-31-30-84.us-west-1.compute.internal>
2020-07-17 18:32:11 +0000

    added files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+
+//routes
+app.get("/",function(req,res){
+   res.render("index.html");
+   //res.send("It works");
+});
+
+app.get("/mercury", function(req,res){
+    res.send("This will be Mercury web page!");
+});
+
+app.get("/venus", function(req,res){
+    res.send("This will be Venus web page!");
+});
+
+app.listen("8080", "127.0.0.1", function() {
+    console.log("express server is running");
+});
