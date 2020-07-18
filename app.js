73746a344792693ed6ee1ec5ef9099339b930e84
EC2 Default User <ec2-user@ip-172-31-30-84.us-west-1.compute.internal>
2020-07-18 23:39:30 +0000

    put navigation to header.ejs

--- app.js
+++ app.js
@@ -6,37 +6,37 @@ app.use(express.static("public"));
 
 //routes
 app.get("/",function(req,res){
-   res.render("index.html");
+   res.render("index.html", {page_name: "home"});
    //res.send("It works");
 });
 
 app.get("/mercury", function(req,res){
-    res.render("mercury.html");
+    res.render("mercury.html", {page_name: "mercury"});
     //res.send("This will be Mercury web page!");
 });
 
 app.get("/venus", function(req,res){
-    res.render("venus.html");
+    res.render("venus.html", {page_name:"venus"});
     //res.send("This will be Venus web page!");
     
 });
 
 app.get("/earth", function(req,res){
-    res.render("earth.html");
+    res.render("earth.html", {page_name:"earth"});
     //res.send("This will be Earth web page!");
     
 });
 
 app.get("/mars", function(req,res){
-    res.render("mars.html");
+    res.render("mars.html", {page_name: "mars"});
     //res.send("This will be Mars web page!");
     
 });
 
-//app.listen("8080", "127.0.0.1", function() {
-   //console.log("express server is running");
-//});
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Running express server...");
+app.listen("8080", "127.0.0.1", function() {
+   console.log("express server is running");
 });
+
+//app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("Running express server...");
+//});
