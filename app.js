fe1f1082e13f5150dda71ef1132e9da7556338f2
EC2 Default User <ec2-user@ip-172-31-30-84.us-west-1.compute.internal>
2020-07-18 21:06:43 +0000

    files updated and changed for final

--- app.js
+++ app.js
@@ -11,17 +11,32 @@ app.get("/",function(req,res){
 });
 
 app.get("/mercury", function(req,res){
-    res.send("This will be Mercury web page!");
+    res.render("mercury.html");
+    //res.send("This will be Mercury web page!");
 });
 
 app.get("/venus", function(req,res){
-    res.send("This will be Venus web page!");
+    res.render("venus.html");
+    //res.send("This will be Venus web page!");
+    
 });
 
-//app.listen("8080", "127.0.0.1", function() {
-//    console.log("express server is running");
-// });
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Running express server...");
+app.get("/earth", function(req,res){
+    res.render("earth.html");
+    //res.send("This will be Earth web page!");
+    
 });
+
+app.get("/mars", function(req,res){
+    res.render("mars.html");
+    //res.send("This will be Mars web page!");
+    
+});
+
+app.listen("8080", "127.0.0.1", function() {
+   console.log("express server is running");
+});
+
+//app.listen(process.env.PORT, process.env.IP, function(){
+    //console.log("Running express server...");
+//});
